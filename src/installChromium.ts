e2c10f3e973f04af305f51cb88eb0e22d315a477
yambal <jy@yambal.net>
2024-02-26 22:22:24 +0900

    installChromium

--- src/installChromium.ts
+++ src/installChromium.ts
@@ -5,36 +5,35 @@ import { checkPuppeteerBinary } from './checkPuppeteerBinary'
 /**
  * Chromiumをインストールする
  */
-export const installChromium = () => {
-  console.group('installChromium()')
-  const pwConf = getPaperbackWriterConfiguration()
+export const installChromium = ():Promise<void> => {
 
-  try {
+  return new Promise((resolve, reject) => {
+    const pwConf = getPaperbackWriterConfiguration()
+
     showMessage({
       message: `[Markdown PDF] Installing Chromium ...`,
       type: 'info'
     })
 
     var statusbarmessage = vscode.window.setStatusBarMessage('$(markdown) Installing Chromium ...')
-
-    // proxy setting
-    setProxy()
-
     var StatusbarMessageTimeout = pwConf.StatusbarMessageTimeout
+
     const puppeteer = require('puppeteer-core')
     const browserFetcher = puppeteer.createBrowserFetcher()
     const revision = "722234" // require(path.join(__dirname, 'node_modules', 'puppeteer-core', 'package.json')).puppeteer.chromium_revision;
     const revisionInfo = browserFetcher.revisionInfo(revision)
 
-    // download Chromium
-    browserFetcher.download(revisionInfo.revision, onProgress)
-      .then(() => browserFetcher.localRevisions())
-      .then(onSuccess)
-      .catch(onError)
+    setProxy()
 
-    function onSuccess(localRevisions: any) {
+    return browserFetcher.download(revisionInfo.revision, (downloadedBytes: number, totalBytes: number) => {
+      var progress = parseInt(`${downloadedBytes / totalBytes * 100}`)
+      vscode.window.setStatusBarMessage('$(markdown) Installing Chromium ' + progress + '%' , StatusbarMessageTimeout)
+    })
+    .then(() => browserFetcher.localRevisions())
+    .then((localRevisions: any) => {
       console.log('Chromium downloaded to ' + revisionInfo.folderPath)
       localRevisions = localRevisions.filter((revision: any) => revision !== revisionInfo.revision)
+
       // Remove previous chromium revisions.
       const cleanupOldVersions = localRevisions.map((revision: any)  => browserFetcher.remove(revision))
 
@@ -43,30 +42,21 @@ export const installChromium = () => {
         vscode.window.setStatusBarMessage('$(markdown) Chromium installation succeeded!', StatusbarMessageTimeout)
         vscode.window.showInformationMessage('[Markdown PDF] Chromium installation succeeded.')
         return Promise.all(cleanupOldVersions)
+        .then(() => {
+          resolve()
+        })
       }
-    }
-
-    function onError(error: any) {
+    })
+    .catch((error: any) => {
       statusbarmessage.dispose()
       vscode.window.setStatusBarMessage('$(markdown) ERROR: Failed to download Chromium!', StatusbarMessageTimeout)
       showMessage({
         message:'Failed to download Chromium! If you are behind a proxy, set the http.proxy option to settings.json and restart Visual Studio Code. See https://github.com/yzane/vscode-markdown-pdf#install',
         type: 'error'
       })
-    }
-
-    function onProgress(downloadedBytes: number, totalBytes: number) {
-      var progress = parseInt(`${downloadedBytes / totalBytes * 100}`)
-      vscode.window.setStatusBarMessage('$(markdown) Installing Chromium ' + progress + '%' , StatusbarMessageTimeout)
-    }
-  } catch (error) {
-    showMessage({
-      message: `installChromium(): ${error}`,
-      type: 'error'
+      reject()
     })
-  } finally {
-    console.groupEnd()
-  }
+  })
 }
 
 function setProxy() {
