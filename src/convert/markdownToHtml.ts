5a2addc3560a248b6c4fd79b07cbdc636d4919f7
yambal <jy@yambal.net>
2024-03-01 16:30:41 +0900

    ejs

--- src/convert/markdownToHtml.ts
+++ src/convert/markdownToHtml.ts
@@ -1,4 +1,7 @@
 import { marked } from 'marked'
+import ejs from 'ejs'
+import { isExistsPath } from '../util'
+import { defautTemplate } from '../templates/defaultTemplate'
 
 export type MarkdownToHtmlProps = {
   markdownString: string
@@ -6,7 +9,12 @@ export type MarkdownToHtmlProps = {
 export const markdownToHtml = async ({markdownString}: MarkdownToHtmlProps): Promise<string> => {
   try {
     console.log(`markdownToHtml({text: ${markdownString.slice(0, 12)}...})`)
-    return await marked(markdownString)
+
+    const htmlBodyString = await marked(markdownString)
+
+    let html = ejs.render(defautTemplate, {body: htmlBodyString})
+
+    return html
   } catch (error: any) {
     console.error('markdownToHtml()', error)
     return ''
